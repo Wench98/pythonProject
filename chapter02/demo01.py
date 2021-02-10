7523b1f9f4abcee131d76ed2d1738025f7f95e4c
Wench98 <627042794@qq.com>
2021-02-10 12:21:58 +0800

    二进制与字符编码 关键字 变量的定义和使用

---
+++ chapter02/demo01.py
@@ -0,0 +1,14 @@
+import keyword
+
+print(chr(0b100111001011000))
+print(ord('乘'))
+
+print(keyword.kwlist)
+
+name = '玛利亚'
+print(name)
+print('标识', id(name))
+print('类型', type(name))
+print('值', name)
+name = 'wench'
+print(name)
