9b899f694154651e0b807ac8b1408c9fb66b5668
Wench98 <627042794@qq.com>
2021-02-24 09:48:14 +0800

    条件表达式、pass 语句

--- chapter03/demo01.py
+++ chapter03/demo01.py
@@ -1,2 +1,10 @@
-present = input('大圣想要什么礼物呢？')
-print(present, type(present))
+num_a = int(input("请输入第一个整数"))
+num_b = int(input("请输入第二个整数"))
+print("使用条件表达式进行比较")
+print((num_a, '大于等于', num_b) if num_a >= num_b else (num_a, '小于', num_b))
+# pass statement
+answer = input('请输入')
+if answer == 'y':
+    pass
+else:
+    pass
