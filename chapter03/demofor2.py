3df65b3abb27a029b55e2e4225cc9eb946962403
Wench98 <627042794@qq.com>
2021-03-02 11:47:11 +0800

    水仙花数练习

---
+++ chapter03/demofor2.py
@@ -0,0 +1,7 @@
+# 打印100 到 1000的水仙花数
+for item in range(100, 1000):
+    ge = item % 10
+    shi = item // 10 % 10
+    bai = item // 100
+    if ge ** 3 + shi ** 3 + bai ** 3 == item:
+        print(item)
